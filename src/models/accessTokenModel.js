2fbbe4eb37cba1457a0159d11de8e4de8157ea22
SanchitTiwari <tiwarisanchit47@gmail.com>
2024-04-29 09:37:36 +0530

    resolving merge conflicts

--- src/models/accessTokenModel.js
+++ src/models/accessTokenModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
+
 // created access token model for handleling login requests by issueing acceess tokens which are valid for 1 hour
+
 const accessTokenSchema = new mongoose.Schema({
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     access_token: { type: String, required: true },
